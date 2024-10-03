ca76bac219be3d7d5a3b13c0a7f277f60d6f6f61
lbasileentando <l.basile@entando.com>
2024-10-03 16:50:42 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import React, { useState } from 'react';
+import TemplateList from './TemplateList';
+import './TemplateList.css';
 
-function App() {
+interface Template {
+  id: string;
+  author: string;
+  state: string;
+  content: string;
+  version: number;
+}
+
+const App: React.FC = () => {
+  const [selectedTemplate, setSelectedTemplate] = useState<Template | undefined>(undefined);
+
+  const handleSelectTemplate = (template: Template) => {
+    setSelectedTemplate(template);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TemplateList onSelectTemplate={handleSelectTemplate} />
     </div>
   );
-}
+};
 
 export default App;
